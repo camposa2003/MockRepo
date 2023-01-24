6942d9d51c2ab86a07970afd21c36ac8ea9c4087
camposa <>
2023-01-24 15:28:57 -0500

    Commit #1

---
+++ MockProject/src/main/java/unit0/mockproject/MockProject.java
@@ -0,0 +1,13 @@
+
+package unit0.mockproject;
+
+/**
+ *
+ * @author camposa <camposa@merrimack.edu>
+ */
+public class MockProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
